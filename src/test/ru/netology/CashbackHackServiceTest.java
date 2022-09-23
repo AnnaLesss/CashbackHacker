59609393b03adf0bcee0f5cdc083f363c50e7c01
AnnaLesss <klatter@mail.ru>
2022-09-23 14:14:37 +0300

    junit4 commit

---
+++ src/test/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    void amount900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void amount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void amount1300() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1300;
+        int expected = 700;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void amount999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+}
